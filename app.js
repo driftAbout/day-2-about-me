f61ce04c909fe95850305e7661f8d67dce617647
Kevin MIller <kevin@driftabout.com>
2017-10-17 18:57:44 -0700

    added initial questions

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var chickenName = prompt('If you were a chicken, what would I call you?');
+console.log('chickenName: ', chickenName);
+
+var huh = prompt('Did you find that to be an odd request? \n Yes or No ( Y/N )');
+console.log('huh: ', huh);
+
+var graced = prompt('Have you ever graced the presence of a professional free range chicken catcher? \n Yes or No ( Y/N )');
+console.log('graced: ', graced);
+
+var zigZag = prompt('When describing the running style of the Gallus gallus domesticus, a subspecies of the red junglefowl, which came first, the zig or the zag?');
+console.log('zigZag: ', zigZag);
+
+var catchCount = prompt('How many chickens do you think I can catch in an hour?');
+console.log('catchCount: ', catchCount);
+
+var road = prompt('Why did the Urban Farmers Chicken Search and Rescue Team member cross the road');
+console.log('road: ', road);
+
+var baseball = ('Do you like baseball? \n Yes or No ( Y/N )');
+console.log('baseball: ', baseball);
+
+alert('Me neither!  You must have played with cousin Bertha, too');
+alert('Really???  You obviously never played with cousin Bertha.');
