dcf64bee357006b5b45a80b0ebabb72453c895d6
Kevin MIller <kevin@driftabout.com>
2017-10-21 17:13:09 -0700

    finished condensing code

--- app.js
+++ app.js
@@ -16,71 +16,58 @@ var keepGoing = true;
 //var keepGoing = false;
 //chickenName = 'Rex Goliath';
 
-questionOne();
-questionTwo();
-questionThree();
-questionFour();
-questionFive();
-questionSix();
-baseballQuestion();
+//initalQuestions [[question, response], [question, response]...]
+var initalQuestions = [['If you were a chicken, what would I call you?', 'I\'m sorry if I offened you.  Namaste...'],[['Did you find that to be an odd request, ', ' ? Yes or No ( Y/N )'], 'Quitter!  Go pluck yourself'], ['I\'m sorry you feel that way.  Would you like to continue anyway?\n\nYes or No ( Y/N )', 'GoodBye...'], ['Have you ever graced the presence of a professional, free range, chicken catcher?\n\nYes or No ( Y/N )', 'Wow... If you were a boat, you would be a big yella chicken ship....'], ['When describing the running style of the Gallus gallus domesticus, a subspecies of the red junglefowl, which came first, the zig or the zag?', 'You must be rolling up that wacky tabaky in that zig zag!'],[['You think like a chicken, ', ' and can\'t answer a simple Zig or Zag question, would you like to continue, anyway?\n\nYes or No ( Y/N )'], 'GoodBye...'], ['How many chickens do you think I can catch in an hour?', 'Too, bad that wasn\'t a number...  I was going to take you for some BBQ....'], ['Why did the Urban Farmers Chicken Search and Rescue Team member cross the road?', 'You know, a chicken would not get away with an answer like that!'], ['One last question.  Do you like baseball?  Yes or No ( Y/N )', 'Oh, I guess one more question was too much'] ];
+
+questionOne(0);
+questionTwo(1);
+questionThree(3);
+questionFour(4);
+questionFive(6);
+questionSix(7);
+baseballQuestion(8);
 questionSeven();
 questionEight();
 questionResults();
 
-function askQuestion(theQuestion){
-  var answer = prompt(theQuestion);
+function askQuestion(question_index){
+  var myQuestion = initalQuestions[question_index][0];
+  console.log('typeof myQuestion: ', typeof myQuestion);
+  if (typeof myQuestion === 'object') {
+    console.log('ARRAY');
+    myQuestion = initalQuestions[question_index][0].join(chickenName);
+  }
+  var answer = prompt(myQuestion);
   if (answer === '' || answer === null) { //abort if canceled or empty
-  //  alert('I\'m sorry if I offened you.  Namaste...');
+    alert(initalQuestions[question_index][1]);
     keepGoing = false;
   }
   return answer;
-
 }
-
 
 ///////////////////////
 // begin name question
-function questionOne() {
-  chickenName = prompt('If you were a chicken, what would I call you?');
+function questionOne(question_number) {
+  chickenName = askQuestion(question_number);
   console.log('chickenName: ', chickenName);
-  if (chickenName === '' || chickenName === null) { //abort if canceled or empty
-    alert('I\'m sorry if I offened you.  Namaste...');
-    keepGoing = false;
-  }
 }
 // end name question
 ///////////////////////
 
 //////////////////////////////////
 // begin main odd request question
-function questionTwo() {
-  console.log('chickenName keepGoing: ', keepGoing);
+function questionTwo(question_number) {
+  console.log('huh keepGoing: ', keepGoing);
+  var huh = askQuestion(question_number);
+
   if (keepGoing){
-    var huh = prompt('Did you find that to be an odd request, ' + chickenName + ' ?  Yes or No ( Y/N )');
-    console.log('huh: ', huh);
-
-    if ((! huh.toLowerCase() === 'n') || (! huh.toLowerCase() === 'no') || (! huh.toLowerCase() === 'y') || (! huh.toLowerCase() === 'yes') ){ //abort if canceled or empty
-      alert('Quitter!  Go pluck yourself');
-      keepGoing = false;
-    }
-
-    console.log('huh keepGoing: ', keepGoing);
-    if (keepGoing){
-      if (huh.toLowerCase() === 'yes' || huh.toLowerCase() === 'y'){ //huh is not yes and not y
-
-        //////////////////////////////////
-        // begin sub odd request question
-        var keepOnTrucking = prompt('I\'m sorry you feel that way.  Would you like to continue anyway?\n\nYes or No ( Y/N )');
-        console.log('keepOnTrucking: ', keepOnTrucking);
-        if (keepOnTrucking.toLowerCase() !== 'yes' && keepOnTrucking.toLowerCase() !== 'y'){ //keepOnTrucking is false
-          console.log('keepTrucking is not yes');
-          alert('GoodBye...');
-          keepGoing = false;
-        }
-        console.log('keepGoing: ', keepGoing);
-        // end sub odd request question
-        ///////////////////////////////
-      }
+    if (huh.toLowerCase() === 'yes' || huh.toLowerCase() === 'y'){
+      //////////////////////////////////
+      // begin sub odd request question
+      askQuestion(2);
+      console.log('keepGoing: ', keepGoing);
+      // end sub odd request question
+      ///////////////////////////////
     }
   }
 }
@@ -89,23 +76,14 @@ function questionTwo() {
 
 /////////////////////////
 //begin graced question
-function questionThree() {
-  if (keepGoing) {
-    var graced = prompt('Have you ever graced the presence of a professional, free range, chicken catcher?\n\nYes or No ( Y/N )');
-    console.log('graced: ', graced);
-
-    if (graced === '' || graced === null ){
-      alert('Wow... If you were a boat, you would be a big yella chicken ship....');
-      keepGoing = false;
-    }
-
-    console.log('graced keepGoing: ', keepGoing);
-    if (keepGoing){
-      if ( graced.toLowerCase() === 'yes' || graced.toLowerCase() === 'y') {
-        alert('Well, I\'ll be the son of my sisters brother!  We must be cousins!');
-      } else {
-        alert('You, aint from around here, ' + chickenName + '  are ya...');
-      }
+function questionThree(question_number) {
+  console.log('graced keepGoing: ', keepGoing);
+  var graced = askQuestion(question_number);
+  if (keepGoing){
+    if ( graced.toLowerCase() === 'yes' || graced.toLowerCase() === 'y') {
+      alert('Well, I\'ll be the son of my sisters brother!  We must be cousins!');
+    } else {
+      alert('You, aint from around here, ' + chickenName + '  are ya...');
     }
   }
 }
@@ -114,31 +92,17 @@ function questionThree() {
 
 ///////////////////////
 //begin zigZag question
-function questionFour() {
+function questionFour(question_number) {
+  var zigZag = askQuestion(question_number);
   if (keepGoing){
-    var zigZag = prompt('When describing the running style of the Gallus gallus domesticus, a subspecies of the red junglefowl, which came first, the zig or the zag?');
-    console.log('zigZag: ', zigZag);
-
-    if (zigZag === '' || zigZag === null){
-      alert('You must be rolling up that wacky tabaky in that zig zag!');
-      keepGoing = false;
-    }
-
-    console.log('gzigZag keepGoing: ', keepGoing);
-    if (keepGoing){
-      if( zigZag.toLowerCase() === 'zig' || zigZag.toLowerCase() === 'zag' ){
-        alert('Trick Question!  They don\'t run from me they get catch-ed');
-      } else {
-        ///////////////////////
-        //begin sub zigZag question
-        var keepOnTrucking = prompt('You think like a chicken, ' + chickenName + ' and can\'t answer a Zig or Zag question, would you like to continue, anyway?\n\nYes or No ( Y/N )');
-        console.log('keepOnTrucking: ', keepOnTrucking);
-        if (!keepOnTrucking.toLowerCase() === 'yes' && !keepOnTrucking.toLowerCase() === 'y'){
-          keepGoing = false;
-        }
-        //end sub zigZag question
-        ///////////////////////
-      }
+    if( zigZag.toLowerCase() === 'zig' || zigZag.toLowerCase() === 'zag' ){
+      alert('Trick Question!  They don\'t run from me they get catch-ed');
+    } else {
+      ///////////////////////
+      //begin sub zigZag question
+      askQuestion(5);
+      //end sub zigZag question
+      ///////////////////////
     }
   }
 }
@@ -147,31 +111,22 @@ function questionFour() {
 
 ////////////////////////////
 //begincatch count question
-function questionFive() {
-  console.log('keepGoing: ', keepGoing);
+function questionFive(question_number) {
+  console.log('catchCount keepGoing: ', keepGoing);
+  var catchCount = askQuestion(question_number);
   if (keepGoing) {
-    var catchCount = prompt('How many chickens do you think I can catch in an hour?');
-    console.log('catchCount: ', catchCount);
-
-    if( catchCount === '' || catchCount === null || ! parseInt(catchCount)){
-      alert('Too, bad that wasn\'t a number...  I was going to take you for some BBQ....');
-      keepGoing = false;
-    }
-    console.log('catchCount keepGoing: ', keepGoing);
-    if (keepGoing === true) {
-      //add this question to the count of questions
-      addQuestion++;
-      var myCatch = 247;
-      if (parseInt(catchCount) < myCatch || parseInt(catchCount) > myCatch){
-        var onlyCatch = '';
-        if (parseInt(catchCount) > myCatch){
-          onlyCatch = 'only ';
-        }
-        alert('Really?  You don\'t know nut\'in \'bout me!. I can ' + onlyCatch + 'catch ' + myCatch + ' chickens in an hour');
-      } else {
-        alert('Hot Damn! Hit my face with hammer!  I can catch exactly ' + myCatch + ' chickens');
-        correctAnswers++;
+    //add this question to the count of questions
+    addQuestion++;
+    var myCatch = 247;
+    if (parseInt(catchCount) < myCatch || parseInt(catchCount) > myCatch){
+      var onlyCatch = '';
+      if (parseInt(catchCount) > myCatch){
+        onlyCatch = 'only ';
       }
+      alert('Really?  You don\'t know nut\'in \'bout me!. I can ' + onlyCatch + 'catch ' + myCatch + ' chickens in an hour');
+    } else {
+      alert('Hot Damn! Hit my face with hammer!  I can catch exactly ' + myCatch + ' chickens');
+      correctAnswers++;
     }
   }
 }
@@ -180,20 +135,10 @@ function questionFive() {
 
 ///////////////////////////////
 //begin cross the road question
-function questionSix() {
-  console.log('keepGoing: ', keepGoing);
+function questionSix(question_number) {
+  var road = askQuestion(question_number);
   if (keepGoing) {
-    var road = prompt('Why did the Urban Farmers Chicken Search and Rescue Team member cross the road?');
-    console.log('road: ', road);
-
-    if (road === '' || road === null ){
-      alert('You know, a chicken would not get away with an answer like that!');
-      keepGoing = false;
-    }
-    console.log('road keepGoing: ', keepGoing);
-    if (keepGoing) {
-      alert('"' + road + '" was a good guess. But the reall answer is because the headless zombie chicken was in pursuit.  Those things run fast with out a head!');
-    }
+    alert('"' + road + '" was a good guess. But the reall answer is because the headless zombie chicken was in pursuit.  Those things run fast with out a head!');
   }
 }
 //end cross the road question
@@ -201,25 +146,17 @@ function questionSix() {
 
 /////////////////////////
 //begin baseball question
-function baseballQuestion() {
-  console.log('baseball: ', keepGoing);
-  if (keepGoing) {
-    var baseball = prompt('One last question.  Do you like baseball?  Yes or No ( Y/N )');
-    console.log('baseball: ', baseball);
-    if(baseball === '' || baseball === null){
-      alert('Oh, I guess one more question was too much');
-      keepGoing = false;
+function baseballQuestion(question_number) {
+  var baseball = askQuestion(question_number);
+  console.log('baseball keepGoing: ', keepGoing);
+  if (keepGoing === true) {
+    if (baseball.toLowerCase() === 'yes' || baseball.toLowerCase() === 'y'){
+      alert('Really???  You obviously never played with cousin Bertha.');
     }
-    console.log('baseball keepGoing: ', keepGoing);
-    if (keepGoing === true) {
-      if (baseball.toLowerCase() === 'yes' || baseball.toLowerCase() === 'y'){
-        alert('Really???  You obviously never played with cousin Bertha.');
-      }
-      else if (baseball.toLowerCase() === 'no' || baseball.toLowerCase() === 'n'){
-        alert('Me neither!  You must have played with cousin Bertha, too.');
-      } else {
-        alert('Yeah, I don\'t buy that....');
-      }
+    else if (baseball.toLowerCase() === 'no' || baseball.toLowerCase() === 'n'){
+      alert('Me neither!  You must have played with cousin Bertha, too.');
+    } else {
+      alert('Yeah, I don\'t buy that....');
     }
   }
 }
